32a08e441f5e58489b3557c22d403f9413d31b90
Jensen Somers <jensen@jsomers.be>
2023-10-22 20:20:56 +0200

    change telescope keymaps

--- lua/jensen/plugins/telescope.lua
+++ lua/jensen/plugins/telescope.lua
@@ -26,11 +26,12 @@ return {
 
     telescope.load_extension("fzf")
 
-    vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-    vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files." })
+    vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Find Git files." })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find Grep." })
     vim.keymap.set("n", "<leader>ps", function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
     end)
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers." })
   end,
 }
