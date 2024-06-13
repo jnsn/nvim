29f50ac158e5f0df73f267e0b18a2f166a1ddb8f
Jensen Somers <jensen@jsomers.be>
2024-06-13 07:21:10 +0200

    chore(deps): update packages

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = { "markdownlint" },
+      },
+    },
+    config = function()
+      local markdownlint = require("lint").linters.markdownlint
+      markdownlint.args = {
+        "--disable",
+        "MD013",
+        "--", -- Required
+      }
+    end,
+  },
+}
